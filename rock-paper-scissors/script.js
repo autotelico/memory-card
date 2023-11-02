7c5362e73bedc182fece0a8cc995f7e4bb392ddc
Henrique Heron <henriqueheron1@gmail.com>
2023-11-02 01:03:04 -0300

    style: center player option button positioning

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,57 +1,62 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const playerSelection = document.querySelector("#player-selection"); 
-    const computerSelection = document.querySelector("#computer-selection");
-    const startButton = document.querySelector("#start-button");
+  const playerSelection = document.querySelector("#player-selection-text"); 
+  const computerSelection = document.querySelector("#computer-selection");
+  const startButton = document.querySelector("#start-button");
 
-    const bgMusic = document.querySelector("#background-music");
-    const toggleMusicButton = document.querySelector("#toggle-music-button");
-    let musicPlaying = true;
+  const bgMusic = document.querySelector("#background-music");
+  const toggleMusicButton = document.querySelector("#toggle-music-button");
+  let musicPlaying = true;
 
-    const playerOptions = document.querySelectorAll(".player-option");
-    const rockButton = document.querySelector("#rock");
-    const paperButton = document.querySelector("#paper");
-    const scissorsButton = document.querySelector("#scissors");
+  const playerOption = document.querySelectorAll(".player-option");
+  const rockButton = document.querySelector("#rock");
+  const paperButton = document.querySelector("#paper");
+  const scissorsButton = document.querySelector("#scissors");
 
-    function getComputerChoice() {
+  function getComputerChoice() {
       computerSelection = Math.floor(Math.random() * 3) + 1;
-      switch(computerSelection) {
-        case 1:
-          return "Rock";
-        case 2:
-          return "Paper";
-        case 3:
-          return "Scissors";
+      switch (computerSelection) {
+          case 1:
+              return "Rock";
+          case 2:
+              return "Paper";
+          case 3:
+              return "Scissors";
       }
-      
-      function getPlayerChoice() {
-        playerSelection
-      }
-      
-    }
-    
-    startButton.addEventListener('click', () => {
+  }
+  
+  startButton.addEventListener('click', () => {
       startButton.style.display = 'none';
       startGame();
-    })
+  });
 
-    function startGame() {
-      playerOptions.forEach(button => {
+  function startGame() {
+      playerOption.forEach(button => {
           button.style.display = 'inline-block';
       });
   }
 
-    toggleMusicButton.addEventListener('click', () => {
+  playerOption.forEach(button => {
+      button.addEventListener('click', () => {
+          getPlayerChoice();
+      });
+  });
+
+  function getPlayerChoice() {
+      playerOption.forEach(button => {
+          button.addEventListener('click', () => {
+              playerSelection = button.id;
+          });
+      });
+  }
+
+  toggleMusicButton.addEventListener('click', () => {
       if (musicPlaying) {
-        bgMusic.pause();
-        musicPlaying = false;
+          bgMusic.pause();
+          musicPlaying = false;
       } else {
-        bgMusic.currentTime = 0;
-        bgMusic.play();
-        musicPlaying = true;
+          bgMusic.currentTime = 0;
+          bgMusic.play();
+          musicPlaying = true;
       }
-      
-    })
-
-  })
-
-  // Make a 'stop music' button with a logo
+  });
+});
